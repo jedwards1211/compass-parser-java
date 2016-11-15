e8af553e882bcae08c6b2e0cae8c4b378ed1fd12
Andy Edwards <jedwards@fastmail.com>
2016-11-15 17:46:31 -0600

    implement inclination conversions

--- src/main/java/org/andork/compass/InclinationUnit.java
+++ src/main/java/org/andork/compass/InclinationUnit.java
@@ -1,5 +1,16 @@
 package org.andork.compass;
 
 public enum InclinationUnit {
-	DEGREES, PERCENT_GRADE, DEGREES_AND_MINUTES, GRADS, DEPTH_GAUGE
+	DEGREES, PERCENT_GRADE, DEGREES_AND_MINUTES, GRADS, DEPTH_GAUGE;
+
+	public static double convert(double value, InclinationUnit toUnit) {
+		switch (toUnit) {
+		case PERCENT_GRADE:
+			return Math.tan(value) * 100;
+		case GRADS:
+			return value * 200 / 180;
+		default:
+			return value;
+		}
+	}
 }
--- src/test/java/org/andork/compass/UnitConversionTests.java
+++ src/test/java/org/andork/compass/UnitConversionTests.java
@@ -12,6 +12,15 @@ public class UnitConversionTests {
 	}
 
 	@Test
+	public void testInclinationConversions() {
+		Assert.assertEquals(100, InclinationUnit.convert(45, InclinationUnit.PERCENT_GRADE), 0.0);
+		Assert.assertEquals(200, InclinationUnit.convert(180, InclinationUnit.GRADS), 0.0);
+		Assert.assertEquals(180, InclinationUnit.convert(180, InclinationUnit.DEGREES), 0.0);
+		Assert.assertEquals(180, InclinationUnit.convert(180, InclinationUnit.DEGREES_AND_MINUTES), 0.0);
+		Assert.assertEquals(180, InclinationUnit.convert(180, InclinationUnit.DEPTH_GAUGE), 0.0);
+	}
+
+	@Test
 	public void testLengthConversions() {
 		Assert.assertEquals(0.3048, LengthUnit.convert(1.0, LengthUnit.METERS), 0.0);
 		Assert.assertEquals(1.0, LengthUnit.convert(1.0, LengthUnit.FEET_AND_INCHES), 0.0);
